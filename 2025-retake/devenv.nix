94c56eb774b16f1d0cd033768ac7e923855b19e1
workflow <4farlion@gmail.com>
2025-06-30 11:48:33 +0100

    local(devenv/2025-retake): init

---
+++ 2025-retake/devenv.nix
@@ -0,0 +1,55 @@
+{pkgs, ...}: {
+  # https://devenv.sh/basics/
+  env.GREET = "Caltech Machine Learning 2025 Retake devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+  ];
+
+  languages.python = {
+    enable = true;
+    uv.enable = true;
+    uv.sync.enable = true;
+    venv.enable = true;
+    venv.requirements = ''
+      jupyterlab
+    '';
+  };
+
+  env.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.libffi}/lib";
+  env.PKG_CONFIG_PATH = "${pkgs.zlib.dev}/lib/pkgconfig:${pkgs.libffi.dev}/lib/pkgconfig";
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    python --version
+    uv --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
---
+++ 2025-retake/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 2025-retake!")
+
+
+if __name__ == "__main__":
+    main()
